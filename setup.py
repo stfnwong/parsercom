a012e708cdef41c534874391720be5bd9177c54f
Stefan Wong <stfnwong@gmail.com>
2020-04-25 14:05:49 +0100

    Changing setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+PARSERCOM SETUP
+"""
+from setuptools import setup
+
+
+setup(name='parsercom',
+      version='0.1',
+      description='A shit parser combinator',
+      url='http://github.com/stfnwong/parsecom',
+      author='Stefan Wong',
+      author_email='stfnwong@gmail.com',
+      license='MIT',
+      packages=['parsercom'],
+    test_suite = 'test',
+      zip_safe=False
+)
